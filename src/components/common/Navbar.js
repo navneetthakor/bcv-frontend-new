aa2a09ecc03cb84353ad6440893f02665f09f0d0
tnavneet8628@gamil.com <tnavneer8628@gmail.com>
2024-06-27 11:41:08 +0530

    navbar profile design

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -46,17 +46,23 @@ export default function Navbar() {
   // navigation buttons
   const navButtons = (txt) => {
     return (
-      <button className="border-transparent hover:border-b-indigo-500 border-2 text-lg font-bold active:text-sm">
+      <button className="border-transparent hover:border-b-indigo-500 border-2 text-lg font-medium active:text-sm">
         {txt}
       </button>
     );
   };
 
+  //  toggle click 
+  const toggleClick = () => {
+    document.getElementById('toolbar').classList.toggle('hidden');
+    document.getElementById('rightarr').classList.toggle('hidden');
+    document.getElementById('bottomarr').classList.toggle('hidden');
+  }
   // list of buttons for center
-  const navLabels = ["Home", "How to use", "Help"];
+  const navLabels = ["Home", "How to use", "Help", "Documentation"];
 
   return (
-    <div className=" relative sm:h-[12vh]  flex flex-row justify-between items-center px-[5vw] pt-2 z-50">
+    <div className=" relative z-[1000] top-0 left-0 sm:h-[11.5vh] border-gray-300 border-b-2 rounded-lg  flex flex-row justify-between items-center px-[5vw] pt-1">
             
 
       {/* logo division  */}
@@ -68,28 +74,34 @@ export default function Navbar() {
       </div>
 
       {/* login button  or avtar */}
-      <div className="h-[100%] sm:w-[20%]  overflow-hidden">
+      <div className="h-[100%] sm:w-[20%]  overflow-hidden flex items-center justify-end">
       {pathIncludesResult ? 
-      <div className="flex flex-row gap-3 items-center">
+      <div onClick={toggleClick} onBlur={toggleClick} className="dropdown dropdown-end flex flex-row gap-3 items-center">
 
         <div class="avatar indicator">
-        <span class="indicator-item indicator-bottom badge badge-primary"></span>
+        <span class="indicator-item indicator-bottom badge badge-primary size-4"></span>
         <div class="w-15 h-[55px] rounded-xl">
           <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
         </div>
       </div>
-
       <div>
         <div className="text-lg">Rohanshu </div>
         <div className="text-bold">rb@contractify.com </div>
         </div>
-        <div className="text-lg"> &#x27A4; </div>
+        <div id="rightarr" className="text-lg "> &#x27A4; </div>
+        <div id="bottomarr" className="text-lg hidden"> &#8659; </div>
       </div>
       :
       
-        <button className="h-[100%] w-[100%] z-10 btn text-white bg-gradient-to-r from-cyan-500 to-blue-500 text-[1.2rem]">Login</button>
+        <button className="h-[70%] w-[50%] z-10 btn text-white bg-gradient-to-r from-cyan-500 to-blue-500 text-[1.2rem]">Login</button>
       }
       </div>
+
+      {/* iteam bar  */}
+      <ul id="toolbar" tabIndex={0} className=" border-gray-300 rounded-sm p-2 border-2 shadow-md w-[15%] absolute top-[12vh] right-[5vw] hidden text-md">
+        <li className="hover:bg-blue-500 hover:text-white"><a>Item 1</a></li>
+        <li className="hover:bg-blue-400"><a>Item 2</a></li>
+      </ul>
     </div>
   );
 }
--- src/scences/ChatBoard.js
+++ src/scences/ChatBoard.js
@@ -3,7 +3,7 @@ import image from "../assets/home.svg";
 import bgimage from "../assets/home_bg.jpg"
 export default function ChatBoard() {
   return (
-    <div  className="w-[100%] h-[100vh] flex justify-center items-center relative z-50">
+    <div  className="w-[100%] h-[100vh] flex justify-center items-center relative z-1">
 
 
       <div class="rounded-lg bg-white border-gray-700 shadow-md border h-[70vh] w-[70vw]">
