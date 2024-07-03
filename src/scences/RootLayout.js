2e1b44fd36f4f61fbae5e879d9acd2d45f60c664
Rohanshu-33 <banodharohanshu@gmail.com>
2024-07-04 00:52:17 +0530

    homepage ui

--- src/scences/RootLayout.js
+++ src/scences/RootLayout.js
@@ -1,16 +1,28 @@
 import React from "react";
 import Navbar from "../components/common/Navbar";
 import { Outlet } from "react-router-dom";
-import bgimage from "../assets/home_bg.jpg"
-import bgimage2 from "../assets//lightBackground2.jpg"
-// import bgimage2 from "../assets//lightBackground.jpg"
+import bgimage from "../assets/home_bg.jpg";
+import bgimage2 from "../assets/lightBackground2.jpg";
 
 export default function RootLayout() {
   return (
-    <div data-theme="mytheme" style={{minHeight: '100vh', width: '100vw'}} >
-      <img src={bgimage} alt="" class="absolute -top-0 -left-0 h-[100vh] w-[100vw]" />
-      <Navbar />
-      <Outlet />
+    <div
+      data-theme="mytheme"
+      style={{
+        minHeight: '100vh',
+        width: '100vw',
+        backgroundImage: `url(${bgimage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        position: 'relative',
+        zIndex: -1,
+      }}
+    >
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <Navbar />
+        <Outlet />
+      </div>
     </div>
   );
 }
